3e81446229d82989a97965d919fa50af540f8092
Judson Paiva <judson.paiva@hotmail.com>
2023-08-01 19:29:26 +0100

    funcao reduce ii

--- array/array_reduce.php
+++ array/array_reduce.php
@@ -9,3 +9,10 @@ function soma($a, $b){
 $resultado = array_reduce($array, "soma");
 
 echo "resultado = $resultado";
+
+// para ordenar basta usar o sort ou o rsort... 
+
+// para ordenar arrays associativos temos outras funcoes
+
+//Nota: Há diferença nos modos de ordenação quando se trata de um array associativo ou nao... 
+
