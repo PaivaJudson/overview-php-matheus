1ab2aefd3c1682a8c7aa64510908ea9cc7d5c365
Judson Paiva <judson.paiva@hotmail.com>
2023-08-01 10:24:47 +0100

    exercicio item de mair valor

---
+++ array_exercicio_str.php
@@ -0,0 +1,24 @@
+<?php
+
+$arr = [
+    'porta' => 100,
+    'macaneta' => 5,
+    'motor' => 2000,
+    'retrovisor' => 8
+];
+
+function itensCaros($arr){
+    $listaItensCaros = [];
+    foreach ($arr as $item => $preco) {
+        if($preco > 10)
+        {
+            array_push($listaItensCaros, $item);
+        }
+    }
+    return $listaItensCaros;
+}
+
+$novoArr = itensCaros($arr);
+echo "<pre>";
+print_r($novoArr);
+
