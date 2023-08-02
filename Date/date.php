f95d176c562cd7e6dd6ff054278e291f65dd1085
Judson Paiva <judson.paiva@hotmail.com>
2023-08-02 23:58:23 +0100

    Date

---
+++ Date/date.php
@@ -0,0 +1,9 @@
+<?php
+
+$dataAtual1 = date("d/m/y");
+$dataAtual2 = date("D/M/Y");
+$dataHora = date("D/M/Y H:i:s");
+
+echo $dataAtual1 . "<br>";
+echo $dataAtual1 . "<br>";
+echo $dataHora . "<br>";
