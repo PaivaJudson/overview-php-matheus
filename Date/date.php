5f67346eb9621be230ebd414db26dc73fac45eb9
Judson Paiva <judson.paiva@hotmail.com>
2023-08-04 22:14:02 +0100

    funcoes de datas setDate e setTime

--- Date/date.php
+++ Date/date.php
@@ -7,3 +7,28 @@ $dataHora = date("D/M/Y H:i:s");
 echo $dataAtual1 . "<br>";
 echo $dataAtual1 . "<br>";
 echo $dataHora . "<br>";
+
+$dataEspecifica = "03-08-2023";
+
+echo strtotime($dataEspecifica);
+echo "<br> ".date("l", strtotime($dataEspecifica)) . "<br>";
+
+
+$data = mktime(0, 0, 0, 05, 12, 1994);
+$dataFormatada = date("d - M - Y", $data);
+
+echo $dataFormatada . "<br>";
+
+$dataAtual = new DateTime();
+// echo "Data Actual: " .$dataAtual; // nao se consegue aceder normalmente a data...
+print_r($dataAtual);
+echo "<br>";
+echo $dataAtual->format("d-M-Y");
+
+// definindo datas
+
+$data = new DateTime();
+$data->setDate(1994, 05, 12);
+$data->setTime(07, 30, 05);
+echo "<pre>";
+print_r($data);
