5ff8b324b61b37b337eebb0561f58eae46590675
Judson Paiva <judson.paiva@hotmail.com>
2023-08-05 23:28:51 +0100

    variaveis globais -

---
+++ PHP_E_WEB/Get.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Formulario - GET</title>
+</head>
+<body>
+        <form action="processamento.php" method="get">
+            <div>
+                <input type="text" name="nome" placeholder="Informe o seu nome">
+            </div>
+            <div>
+                <input type="submit" value="Enviar">
+            </div>
+        </form>
+</body>
+</html>
