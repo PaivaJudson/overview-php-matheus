9b49aa226a818f1f0ca8582266f20fcaab8d0897
Judson Paiva <judson.paiva@hotmail.com>
2023-08-06 23:44:50 +0100

    envio e recepcao de dados - files

---
+++ PHP_E_WEB/preenchimento.php
@@ -0,0 +1,34 @@
+<?php
+
+$usuarios = [
+    'nome'      => 'Judson Paiva',
+    'idade'     => 29,
+    'profissao' => 'Programador'
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="">
+        <div>
+            Nome: <input type="text" name="nome" value="<?=$usuarios['nome']?>">
+        </div>
+        <div>
+            Idade: <input type="text" name="idade" value="<?= $usuarios['idade'] ?>">
+        </div>
+        <div>
+            Profissao: <input type="text" name="" value="<?= $usuarios['profissao'] ?>">
+        </div>
+    </form>
+</body>
+
+</html>
---
+++ PHP_E_WEB/upload.php
@@ -0,0 +1,23 @@
+<?php
+
+    print_r($_FILES);
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="upload.php" method="POST" enctype="multipart/form-data">
+        <div>
+            <input type="file" name="imagem">
+        </div>
+        <input type="submit" value="Enviar">
+    </form>
+</body>
+</html>
