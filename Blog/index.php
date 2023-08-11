12aa061d2cc8f107ac23a7e04cb0e920b63f0c06
Judson Paiva <judson.paiva@hotmail.com>
2023-08-11 23:50:14 +0100

    inicializacao do Project Blog - folder helpers

---
+++ Blog/index.php
@@ -0,0 +1,16 @@
+<?php
+    include_once "helpers/url.php";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Codar</title>
+    <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
+</head>
+<body>
+    
+</body>
+</html>
