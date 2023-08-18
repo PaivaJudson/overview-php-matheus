a507c56b508c20f3675421da8a8f3461876684e2
Judson Paiva <judson.paiva@hotmail.com>
2023-08-18 01:42:37 +0100

    finalizando o css do post.php sem responsividade

--- Blog/post.php
+++ Blog/post.php
@@ -16,7 +16,7 @@ if (isset($_GET['id'])) {
 ?>
 
 
-<main id="posts-container">
+<main id="post-container">
 
     <div class="content-container">
         <h1 id="main-title"><?= $currentPost['title'] ?></h1>
@@ -24,43 +24,35 @@ if (isset($_GET['id'])) {
         <div class="img-container">
             <img src="<?= $BASE_URL ?>/img/<?= $currentPost['img'] ?>" alt="<?= $currentPost['title'] ?>">
         </div>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis, magnam commodi veritatis deleniti fugit porro nisi in ducimus totam placeat quis, sapiente, nostrum eaque cum quasi cumque esse maiores necessitatibus.
+        <p class="post-content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis, magnam commodi veritatis deleniti fugit porro nisi in ducimus totam placeat quis, sapiente, nostrum eaque cum quasi cumque esse maiores necessitatibus.
             Inventore, a odit. Excepturi expedita perferendis explicabo molestias ipsum quia unde, fugit architecto, cum eum nulla obcaecati voluptates repellat magnam molestiae aut tempora officia sequi? Tempore suscipit totam repudiandae labore?
             Doloremque labore itaque corrupti, neque perspiciatis incidunt debitis ab deleniti temporibus autem omnis quaerat similique aliquam, qui sunt vitae nulla recusandae iusto? Minima ab quibusdam incidunt veritatis consectetur error enim.
             Adipisci dolorum cumque officia optio. Similique ipsum corporis nobis nam, dolor perspiciatis nisi fugit odit dignissimos et inventore a vero ducimus non quod dolorum minima. Corporis dicta cumque laboriosam culpa?
             Rerum alias quo provident nisi, voluptate sint nulla facere, officia, officiis neque veniam a unde nobis ex vitae omnis iste dicta doloribus temporibus dolor corrupti culpa qui. Quas, quis delectus!
         </p>
-        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis, magnam commodi veritatis deleniti fugit porro nisi in ducimus totam placeat quis, sapiente, nostrum eaque cum quasi cumque esse maiores necessitatibus.
+        <p class="post-content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis, magnam commodi veritatis deleniti fugit porro nisi in ducimus totam placeat quis, sapiente, nostrum eaque cum quasi cumque esse maiores necessitatibus.
             Inventore, a odit. Excepturi expedita perferendis explicabo molestias ipsum quia unde, fugit architecto, cum eum nulla obcaecati voluptates repellat magnam molestiae aut tempora officia sequi? Tempore suscipit totam repudiandae labore?
             Doloremque labore itaque corrupti, neque perspiciatis incidunt debitis ab deleniti temporibus autem omnis quaerat similique aliquam, qui sunt vitae nulla recusandae iusto? Minima ab quibusdam incidunt veritatis consectetur error enim.
             Adipisci dolorum cumque officia optio. Similique ipsum corporis nobis nam, dolor perspiciatis nisi fugit odit dignissimos et inventore a vero ducimus non quod dolorum minima. Corporis dicta cumque laboriosam culpa?
             Rerum alias quo provident nisi, voluptate sint nulla facere, officia, officiis neque veniam a unde nobis ex vitae omnis iste dicta doloribus temporibus dolor corrupti culpa qui. Quas, quis delectus!
         </p>
     </div>
+
+    <aside id="nav-container">
+        <h3 id="tags-title">Tags</h3>
+        <ul id="tag-list">
+            <?php foreach ($currentPost['tags'] as $tags) : ?>
+                <li><a href="#"><?= $tags ?></a></li>
+            <?php endforeach ?>
+        </ul>
+        <h3 id="categories-title">Categorias</h3>
+        <ul id="categories-list">
+            <?php foreach($categories as $category) : ?>
+                <li><a href="#"><?= $category ?></a></li>
+            <?php endforeach ?>
+        </ul>
+    </aside>
 </main>
-
-<aside id="nav-container">
-    <h3 id="tags-title">Tags</h3>
-    <ul class="tag-list">
-        <?php foreach ($currentPost['tags'] as $tags) : ?>
-            <li><a href="#"><?= $tags ?></a></li>
-        <?php endforeach ?>
-    </ul>
-    <h3 id="categories-title">Categorias</h3>
-    <ul class="categories-list">
-        <?php foreach ($categories['tags'] as $category) : ?>
-            <li><a href="#"><?= $category ?></a></li>
-        <?php endforeach ?>
-    </ul>
-</aside>
-
-
-
-
-
-
-
-
 
 <?php
 include_once "templates/footer.php";
--- Blog/templates/header.php
+++ Blog/templates/header.php
@@ -1,7 +1,7 @@
 <?php
 include_once "helpers/url.php";
 include_once "data/posts.php";
-include_once "data/categories";
+include_once "data/categories.php";
 ?>
 
 <!DOCTYPE html>
